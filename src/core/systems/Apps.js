57be5c4e90500dcfa8d19a1d9d7e1aa24387fdda
peezy <itspeezy@protonmail.com>
2025-03-09 10:04:23 -0300

    world_envs v1.0.1

--- src/core/systems/Apps.js
+++ src/core/systems/Apps.js
@@ -9,6 +9,9 @@ import { ControlPriorities } from '../extras/ControlPriorities'
 import { warn } from '../extras/warn'
 
 const internalEvents = ['fixedUpdate', 'updated', 'lateUpdate', 'enter', 'leave', 'chat', 'health']
+const worldEnvs = [...Object.entries(process.env)]
+  .filter(([key, val]) => key.startsWith('WORLD_'))
+  .reduce((obj, [kkey, vval]) => ({ ...obj, [kkey]: vval }), {})
 
 /**
  * Apps System
@@ -36,6 +39,10 @@ export class Apps extends System {
       },
       getIsClient(entity) {
         return world.network.isClient
+      },
+      getEnvs(entity) {
+        return world.network.isServer ? worldEnvs : 
+        console.log('some meaningless update...')
       },
       add(entity, pNode) {
         const node = getRef(pNode)
