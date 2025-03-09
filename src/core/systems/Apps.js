c70c9757d192d6c43db64a82370d637da817133e
peezy <itspeezy@protonmail.com>
2025-03-09 09:03:19 -0300

    app_sendTo v1.0.3

--- src/core/systems/Apps.js
+++ src/core/systems/Apps.js
@@ -192,6 +192,11 @@ export class Apps extends System {
         const event = [entity.data.id, entity.blueprint.version, name, data]
         world.network.send('entityEvent', event, ignoreSocketId)
       },
+      sendTo(nid, name, data) {
+        if (world.network.isClient) return // client cant send events to other clients, unless...?
+        const event = [entity.data.id, entity.blueprint.version, name, data]
+        world.network.sendTo(nid, 'entityEvent', event)
+      },
       emit(entity, name, data) {
         if (internalEvents.includes(name)) {
           return console.error(`apps cannot emit internal events (${name})`)
