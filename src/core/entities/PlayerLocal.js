2064cb65bd40fa65bdfb53bc97d301f26a337fc6
saori-eth <d84da@proton.me>
2025-03-28 01:12:14 -0500

    clean

--- src/core/entities/PlayerLocal.js
+++ src/core/entities/PlayerLocal.js
@@ -994,7 +994,6 @@ export class PlayerLocal extends Entity {
       this.data.health = data.health
       this.nametag.health = data.health
       this.world.events.emit('health', { playerId: this.data.id, health: data.health })
-      console.log('modify', data.health)
       // changed = true
     }
     if (data.hasOwnProperty('avatar')) {
